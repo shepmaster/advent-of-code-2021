b96f6b4d6855e8e8ddafdab3948c6057bf4968b5
Jake Goulding <jake.goulding@gmail.com>
2021-12-03 10:58:14 -0500

    day 01 - part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,12 +3,23 @@ use itertools::Itertools;
 const INPUT: &str = include_str!("../input");
 
 fn main() {
-    println!("{}", number_increases(INPUT));
+    println!("part 1: {}", number_increases(INPUT));
+    println!("part 2: {}", number_window_increases(INPUT));
 }
 
 fn number_increases(s: &str) -> usize {
     s.lines()
         .flat_map(|l| l.trim().parse::<u32>())
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+fn number_window_increases(s: &str) -> usize {
+    s.lines()
+        .flat_map(|l| l.trim().parse::<u32>())
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
         .tuple_windows()
         .filter(|(a, b)| b > a)
         .count()
@@ -32,7 +43,12 @@ mod test {
 "#;
 
     #[test]
-    fn test1() {
+    fn test_part1() {
         assert_eq!(7, number_increases(TEST_INPUT_1));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(5, number_window_increases(TEST_INPUT_1));
     }
 }
