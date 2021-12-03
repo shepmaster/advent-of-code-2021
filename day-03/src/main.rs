35d5dae51a0a45594c6e90c487e926fae945715b
Jake Goulding <jake.goulding@gmail.com>
2021-12-03 14:28:41 -0500

    day 03 - part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -5,16 +5,13 @@ const INPUT: &str = include_str!("../input");
 fn main() {
     // Not 17640 -- real numbers are > 5 bits
     println!("part1: {}", gamma_epsilon_product(INPUT));
+    println!("part2: {}", life_support_rating(INPUT));
 }
 
 fn gamma_epsilon_product(s: &str) -> u64 {
     let mut map = BTreeMap::<_, i32>::new();
 
-    for l in s.lines() {
-        let l = l.trim();
-        if l.is_empty() {
-            continue;
-        }
+    for l in clean_lines(s) {
         for (p, c) in l.chars().enumerate() {
             let delta = if c == '1' { 1 } else { -1 };
             *map.entry(p).or_default() += delta;
@@ -41,6 +38,47 @@ fn gamma_epsilon_product(s: &str) -> u64 {
     omega * gamma
 }
 
+fn life_support_rating(s: &str) -> u64 {
+    fn delve<'a>(lines: &[&'a str], prefer_one: bool, depth: usize) -> &'a str {
+        // Exit if we only have one string
+        if let Some((one, rest)) = lines.split_first() {
+            if rest.is_empty() {
+                return one;
+            }
+        }
+
+        let (bit_0, bit_1): (Vec<_>, Vec<_>) = lines
+            .iter()
+            .partition(|l| l.chars().nth(depth) == Some('0'));
+
+        use std::cmp::Ordering::*;
+        let selected = match (bit_0.len().cmp(&bit_1.len()), prefer_one) {
+            (Less, true) => bit_1,
+            (Equal, true) => bit_1,
+            (Greater, true) => bit_0,
+
+            (Less, false) => bit_0,
+            (Equal, false) => bit_0,
+            (Greater, false) => bit_1,
+        };
+
+        delve(&selected, prefer_one, depth + 1)
+    }
+
+    let lines: Vec<_> = clean_lines(s).collect();
+    let oxygen = delve(&lines, true, 0);
+    let co2 = delve(&lines, false, 0);
+
+    let oxygen = u64::from_str_radix(oxygen, 2).expect("Not binary");
+    let co2 = u64::from_str_radix(co2, 2).expect("Not binary");
+
+    oxygen * co2
+}
+
+fn clean_lines(s: &str) -> impl Iterator<Item = &str> {
+    s.lines().map(str::trim).filter(|s| !s.is_empty())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -63,5 +101,10 @@ mod test {
     #[test]
     fn test_part_1() {
         assert_eq!(198, gamma_epsilon_product(TEST_INPUT));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(230, life_support_rating(TEST_INPUT));
     }
 }
