9d1a3d5bcb7626d6378e339be44c261bf8c4fb42
Jake Goulding <jake.goulding@gmail.com>
2021-12-07 11:08:10 -0500

    day 07 - part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -5,17 +5,21 @@ use std::collections::BTreeMap;
 const INPUT: &str = include_str!("../input");
 
 fn main() {
-    println!("part1: {}", minimum_alignment_fuel(INPUT));
+    println!("part1: {}", minimum_alignment_fuel(INPUT, fuel_cost_linear));
+    println!("part2: {}", minimum_alignment_fuel(INPUT, fuel_cost_ramped));
 }
 
-fn minimum_alignment_fuel(s: &str) -> u64 {
-    minimum_alignment_fuel_inner(s).unwrap()
+fn minimum_alignment_fuel(s: &str, fuel_cost: impl Fn(&Positions, Coord) -> u64) -> u64 {
+    minimum_alignment_fuel_inner(s, fuel_cost).unwrap()
 }
 
 type Coord = u32;
 type Positions = BTreeMap<Coord, u64>;
 
-fn minimum_alignment_fuel_inner(s: &str) -> Result<u64> {
+fn minimum_alignment_fuel_inner(
+    s: &str,
+    fuel_cost: impl Fn(&Positions, Coord) -> u64,
+) -> Result<u64> {
     let mut positions = Positions::new();
     for pos in s.split(',').map(|p| p.trim().parse()) {
         let pos = pos?;
@@ -38,11 +42,29 @@ fn minimum_alignment_fuel_inner(s: &str) -> Result<u64> {
         .map_err(Into::into)
 }
 
-fn fuel_cost(positions: &Positions, destination: Coord) -> u64 {
-    positions
-        .iter()
-        .map(|(pos, count)| u64::from(Coord::abs_diff(*pos, destination)) * count)
-        .sum()
+fn fuel_cost_linear(positions: &Positions, destination: Coord) -> u64 {
+    sum_fuel_computation(positions, |pos| {
+        u64::from(Coord::abs_diff(pos, destination))
+    })
+}
+
+fn fuel_cost_ramped(positions: &Positions, destination: Coord) -> u64 {
+    sum_fuel_computation(positions, |pos| {
+        let dist = u64::from(Coord::abs_diff(pos, destination));
+        inclusive_sum_down_to_zero(dist)
+    })
+}
+
+fn sum_fuel_computation(positions: &Positions, f: impl Fn(Coord) -> u64) -> u64 {
+    positions.iter().map(|(&pos, &count)| f(pos) * count).sum()
+}
+
+fn inclusive_sum_down_to_zero(value: u64) -> u64 {
+    if value % 2 == 0 {
+        (value / 2) * (value + 1)
+    } else {
+        inclusive_sum_down_to_zero(value - 1) + value
+    }
 }
 
 type Error = Box<dyn std::error::Error>;
@@ -56,6 +78,19 @@ mod test {
 
     #[test]
     fn test_part1() {
-        assert_eq!(37, minimum_alignment_fuel(TEST_INPUT));
+        assert_eq!(37, minimum_alignment_fuel(TEST_INPUT, fuel_cost_linear));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(168, minimum_alignment_fuel(TEST_INPUT, fuel_cost_ramped));
+    }
+
+    #[test]
+    fn test_inclusive_sum_down_to_zero() {
+        for v in 0..=100 {
+            let oracle: u64 = (0..=v).sum();
+            assert_eq!(oracle, inclusive_sum_down_to_zero(v), "For value {v}");
+        }
     }
 }
