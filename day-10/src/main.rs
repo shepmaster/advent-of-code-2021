94aae112c5611e08bf8bf80ba8741a138d17cd94
Jake Goulding <jake.goulding@gmail.com>
2021-12-10 10:05:58 -0500

    day 10 - part 1

---
+++ day-10/src/main.rs
@@ -0,0 +1,109 @@
+use snafu::{ensure, OptionExt, Snafu};
+
+const INPUT: &str = include_str!("../input");
+
+fn main() {
+    println!("part1: {}", syntax_error_score(INPUT));
+}
+
+fn syntax_error_score(s: &str) -> u32 {
+    syntax_error_score_inner(s).unwrap()
+}
+
+fn syntax_error_score_inner(s: &str) -> Result<u32> {
+    let points = s.lines().map(|l| {
+        match parse_line(l.trim()) {
+            Ok(_) => Ok(0), // ignored
+            Err(Error::Malformed { c }) => Ok(c.points().into()),
+            Err(e) => Err(e),
+        }
+    });
+
+    itertools::process_results(points, |p| p.sum())
+}
+
+fn parse_line(s: &str) -> Result<Vec<Char>> {
+    use OpenClose::*;
+
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        match OpenClose::from_char(c)? {
+            Open(v) => stack.push(v),
+            Close(v) => {
+                let open = stack.pop().context(ExtraClosingSnafu)?;
+                ensure!(open == v, MalformedSnafu { c: v });
+            }
+        }
+    }
+    Ok(stack)
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum OpenClose {
+    Open(Char),
+    Close(Char),
+}
+
+impl OpenClose {
+    fn from_char(c: char) -> Result<Self> {
+        use {Char::*, OpenClose::*};
+
+        Ok(match c {
+            '{' => Open(CurlyBoi),
+            '}' => Close(CurlyBoi),
+
+            '(' => Open(RoundBoi),
+            ')' => Close(RoundBoi),
+
+            '[' => Open(SquareBoi),
+            ']' => Close(SquareBoi),
+
+            '<' => Open(PointyBoi),
+            '>' => Close(PointyBoi),
+
+            _ => return UnknownSnafu { c }.fail(),
+        })
+    }
+}
+
+#[allow(clippy::enum_variant_names)] // yeah boi
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Char {
+    CurlyBoi,
+    RoundBoi,
+    SquareBoi,
+    PointyBoi,
+}
+
+impl Char {
+    fn points(&self) -> u16 {
+        use Char::*;
+        match self {
+            RoundBoi => 3,
+            SquareBoi => 57,
+            CurlyBoi => 1197,
+            PointyBoi => 25137,
+        }
+    }
+}
+
+#[derive(Debug, Snafu)]
+enum Error {
+    ExtraClosing,
+    Malformed { c: Char },
+    Unknown { c: char },
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test-input");
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(26397, syntax_error_score(TEST_INPUT));
+    }
+}
