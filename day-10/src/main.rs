3b7723214d77c9f0e08ca7476b9f232d1d52a9ac
Jake Goulding <jake.goulding@gmail.com>
2021-12-10 10:17:24 -0500

    day 10 - part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -4,6 +4,7 @@ const INPUT: &str = include_str!("../input");
 
 fn main() {
     println!("part1: {}", syntax_error_score(INPUT));
+    println!("part2: {}", autocomplete_score(INPUT));
 }
 
 fn syntax_error_score(s: &str) -> u32 {
@@ -14,12 +15,40 @@ fn syntax_error_score_inner(s: &str) -> Result<u32> {
     let points = s.lines().map(|l| {
         match parse_line(l.trim()) {
             Ok(_) => Ok(0), // ignored
-            Err(Error::Malformed { c }) => Ok(c.points().into()),
+            Err(Error::Malformed { c }) => Ok(c.syntax_error_points().into()),
             Err(e) => Err(e),
         }
     });
 
     itertools::process_results(points, |p| p.sum())
+}
+
+fn autocomplete_score(s: &str) -> u64 {
+    autocomplete_score_inner(s).unwrap()
+}
+
+fn autocomplete_score_inner(s: &str) -> Result<u64> {
+    let points = s.lines().map(|l| {
+        match parse_line(l.trim()) {
+            Ok(s) => Ok(Some(s)),
+            Err(Error::Malformed { .. }) => Ok(None), // ignored
+            Err(e) => Err(e),
+        }
+    });
+
+    itertools::process_results(points, |p| {
+        let mut scores: Vec<_> = p
+            .flatten()
+            .map(|s| {
+                s.into_iter()
+                    .rev()
+                    .fold(0, |acc, c| acc * 5 + u64::from(c.autocomplete_points()))
+            })
+            .collect();
+
+        scores.sort_unstable();
+        scores[scores.len() / 2]
+    })
 }
 
 fn parse_line(s: &str) -> Result<Vec<Char>> {
@@ -76,13 +105,23 @@ enum Char {
 }
 
 impl Char {
-    fn points(&self) -> u16 {
+    fn syntax_error_points(&self) -> u16 {
         use Char::*;
         match self {
             RoundBoi => 3,
             SquareBoi => 57,
             CurlyBoi => 1197,
             PointyBoi => 25137,
+        }
+    }
+
+    fn autocomplete_points(&self) -> u16 {
+        use Char::*;
+        match self {
+            RoundBoi => 1,
+            SquareBoi => 2,
+            CurlyBoi => 3,
+            PointyBoi => 4,
         }
     }
 }
@@ -105,5 +144,10 @@ mod test {
     #[test]
     fn test_part_1() {
         assert_eq!(26397, syntax_error_score(TEST_INPUT));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(288957, autocomplete_score(TEST_INPUT));
     }
 }
