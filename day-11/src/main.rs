1949f612c763fcc670b58470378db69d75b0b208
Jake Goulding <jake.goulding@gmail.com>
2021-12-11 14:35:09 -0500

    day 11 - part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -5,7 +5,8 @@ use std::collections::{BTreeMap, BTreeSet};
 const INPUT: &str = include_str!("../input");
 
 fn main() {
-    println!("part1: {}", n_flashes(INPUT, 100));
+    println!("part1: {}", n_flashes(INPUT, 1000));
+    println!("part2: {}", first_simultaneous_flash(INPUT));
 }
 
 fn n_flashes(s: &str, iterations: usize) -> usize {
@@ -13,8 +14,26 @@ fn n_flashes(s: &str, iterations: usize) -> usize {
 }
 
 fn n_flashes_inner(s: &str, iterations: usize) -> Result<usize> {
-    let mut board = s
-        .trim()
+    let mut board = parse_board(s)?;
+    Ok((0..iterations).map(|_| step(&mut board).len()).sum())
+}
+
+fn first_simultaneous_flash(s: &str) -> usize {
+    first_simultaneous_flash_inner(s).unwrap()
+}
+
+fn first_simultaneous_flash_inner(s: &str) -> Result<usize> {
+    let mut board = parse_board(s)?;
+    (1..)
+        .find(|_| step(&mut board).len() == board.len())
+        .ok_or_else(|| "Never found a simultaneous flash".into())
+}
+
+type Coord = (usize, usize);
+type Board = BTreeMap<Coord, u32>;
+
+fn parse_board(s: &str) -> Result<Board> {
+    s.trim()
         .lines()
         .enumerate()
         .flat_map(|(y, l)| {
@@ -23,48 +42,42 @@ fn n_flashes_inner(s: &str, iterations: usize) -> Result<usize> {
                 Some(((x, y), c))
             })
         })
-        .collect::<Option<BTreeMap<_, _>>>()
-        .ok_or("Invalid digit")?;
+        .collect::<Option<_>>()
+        .ok_or_else(|| "Invalid digit".into())
+}
 
-    let mut total_flashed = 0;
+fn step(board: &mut Board) -> BTreeSet<Coord> {
+    let mut intermediate_flashed = BTreeSet::new();
+    let mut flashed = BTreeSet::new();
 
-    for _ in 0..iterations {
-        let mut intermediate_flashed = BTreeSet::new();
-        let mut flashed = BTreeSet::new();
-
-        for (&c, v) in &mut board {
-            *v += 1;
-            if *v > 9 {
-                intermediate_flashed.insert(c);
-            }
+    for (&c, v) in &mut *board {
+        *v += 1;
+        if *v > 9 {
+            intermediate_flashed.insert(c);
         }
+    }
 
-        while let Some(c) = intermediate_flashed.pop_first() {
-            flashed.insert(c);
+    while let Some(c) = intermediate_flashed.pop_first() {
+        flashed.insert(c);
 
-            for n in neighbors(c) {
-                if let Some(v) = board.get_mut(&n) {
-                    *v += 1;
-                    if *v > 9 && !flashed.contains(&n) {
-                        intermediate_flashed.insert(n);
-                    }
+        for n in neighbors(c) {
+            if let Some(v) = board.get_mut(&n) {
+                *v += 1;
+                if *v > 9 && !flashed.contains(&n) {
+                    intermediate_flashed.insert(n);
                 }
             }
         }
-
-        for c in &flashed {
-            if let Some(v) = board.get_mut(c) {
-                *v = 0;
-            }
-        }
-
-        total_flashed += flashed.len();
     }
 
-    Ok(total_flashed)
-}
+    for c in &flashed {
+        if let Some(v) = board.get_mut(c) {
+            *v = 0;
+        }
+    }
 
-type Coord = (usize, usize);
+    flashed
+}
 
 fn neighbors((x, y): Coord) -> impl Iterator<Item = Coord> {
     let x0 = x.checked_sub(1);
@@ -98,5 +111,10 @@ mod test {
     #[test]
     fn test_part_1() {
         assert_eq!(1656, n_flashes(TEST_INPUT, 100));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(195, first_simultaneous_flash(TEST_INPUT));
     }
 }
