9b49ad580634134225e201c92d2bca758c237886
Jake Goulding <jake.goulding@gmail.com>
2021-12-12 13:12:08 -0500

    day 12 - part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(map_first_last)]
+#![deny(rust_2018_idioms)]
 
 use std::collections::{BTreeMap, BTreeSet};
 
@@ -6,6 +7,7 @@ const INPUT: &str = include_str!("../input");
 
 fn main() {
     println!("part1: {}", n_paths(INPUT));
+    println!("part2: {}", n_paths_one_dupe(INPUT));
 }
 
 fn n_paths(s: &str) -> usize {
@@ -13,6 +15,46 @@ fn n_paths(s: &str) -> usize {
 }
 
 fn n_paths_inner(s: &str) -> Result<usize> {
+    let graph = parse_graph(s)?;
+
+    Ok(traverse_graph(graph, |candidate, next_node| {
+        if next_node == "start" {
+            return false;
+        }
+        if !is_little_cave(next_node) {
+            return true;
+        }
+        !candidate.contains(&next_node)
+    })
+    .len())
+}
+
+fn n_paths_one_dupe(s: &str) -> usize {
+    n_paths_one_dupe_inner(s).unwrap()
+}
+
+fn n_paths_one_dupe_inner(s: &str) -> Result<usize> {
+    let graph = parse_graph(s)?;
+
+    Ok(traverse_graph(graph, |candidate, next_node| {
+        if next_node == "start" {
+            return false;
+        }
+        if !is_little_cave(next_node) {
+            return true;
+        }
+        if !has_duplicate_little_cave(candidate) {
+            return true;
+        }
+        !candidate.contains(&next_node)
+    })
+    .len())
+}
+
+type Graph<'a> = BTreeMap<&'a str, BTreeSet<&'a str>>;
+type Path<'a> = Vec<&'a str>;
+
+fn parse_graph(s: &str) -> Result<Graph<'_>> {
     let mut graph = BTreeMap::<_, BTreeSet<_>>::new();
 
     for l in s.trim().lines() {
@@ -21,6 +63,13 @@ fn n_paths_inner(s: &str) -> Result<usize> {
         graph.entry(r).or_default().insert(l);
     }
 
+    Ok(graph)
+}
+
+fn traverse_graph(
+    graph: Graph<'_>,
+    allow_next_node: impl Fn(&Path<'_>, &str) -> bool,
+) -> BTreeSet<Path<'_>> {
     let mut to_visit = BTreeSet::from_iter([vec!["start"]]);
     let mut paths = BTreeSet::new();
 
@@ -30,19 +79,36 @@ fn n_paths_inner(s: &str) -> Result<usize> {
             paths.insert(candidate);
         } else {
             for &next_node in &graph[last] {
-                let is_little_cave = next_node.chars().all(|c| c.is_ascii_lowercase());
-                if is_little_cave && candidate.contains(&next_node) {
-                    continue;
+                if allow_next_node(&candidate, next_node) {
+                    let mut next_path = candidate.clone();
+                    next_path.push(next_node);
+                    to_visit.insert(next_path);
                 }
-
-                let mut next_path = candidate.clone();
-                next_path.push(next_node);
-                to_visit.insert(next_path);
             }
         }
     }
 
-    Ok(paths.len())
+    paths
+}
+
+fn has_duplicate_little_cave(candidate: &Path<'_>) -> bool {
+    let mut candidate = &candidate[..];
+
+    while let Some((h, next_candidate)) = candidate.split_first() {
+        candidate = next_candidate;
+        if !is_little_cave(h) {
+            continue;
+        }
+        if candidate.contains(h) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn is_little_cave(node: &str) -> bool {
+    node.chars().all(|c| c.is_ascii_lowercase())
 }
 
 type Error = Box<dyn std::error::Error>;
@@ -61,5 +127,12 @@ mod test {
         assert_eq!(10, n_paths(TEST_INPUT_0));
         assert_eq!(19, n_paths(TEST_INPUT_1));
         assert_eq!(226, n_paths(TEST_INPUT_2));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(36, n_paths_one_dupe(TEST_INPUT_0));
+        assert_eq!(103, n_paths_one_dupe(TEST_INPUT_1));
+        assert_eq!(3509, n_paths_one_dupe(TEST_INPUT_2));
     }
 }
