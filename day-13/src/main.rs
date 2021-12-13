ca7ec27d3062bd944e04ce1a33d9083762a8d1cd
Jake Goulding <jake.goulding@gmail.com>
2021-12-13 10:31:17 -0500

    day 13 - part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,16 +1,38 @@
-use std::collections::BTreeSet;
+use std::{cmp::max, collections::BTreeSet};
 
 const INPUT: &str = include_str!("../input");
 
 fn main() {
     println!("part1: {}", dots_visible_after_folds(INPUT, 1));
+
+    // WRONG: BLKJRBAE; BLKJRBAC
+    // Wasn't including max_x, so right-side was cut off
+    println!("part2:");
+    dots_picture(INPUT);
 }
 
 fn dots_visible_after_folds(s: &str, limit: usize) -> usize {
     let mut lines = s.lines();
 
-    let mut grid: BTreeSet<(i32, i32)> = lines
-        .by_ref()
+    let mut grid = parse_grid(&mut lines);
+    fold_paper(&mut grid, lines.take(limit));
+
+    grid.len()
+}
+
+fn dots_picture(s: &str) {
+    let mut lines = s.lines();
+
+    let mut grid = parse_grid(&mut lines);
+    fold_paper(&mut grid, lines);
+
+    print_grid(&grid);
+}
+
+type Grid = BTreeSet<(i32, i32)>;
+
+fn parse_grid<'a>(lines: impl Iterator<Item = &'a str>) -> Grid {
+    lines
         .map(|l| l.trim())
         .take_while(|l| !l.is_empty())
         .map(|l| {
@@ -19,11 +41,13 @@ fn dots_visible_after_folds(s: &str, limit: usize) -> usize {
             let y = y.parse().expect("malformed y");
             (x, y)
         })
-        .collect();
+        .collect()
+}
 
+fn fold_paper<'a>(grid: &mut Grid, lines: impl Iterator<Item = &'a str>) {
     let mut to_move = Vec::with_capacity(grid.len());
 
-    for fold in lines.take(limit) {
+    for fold in lines {
         let fold = fold.trim_start_matches("fold along ");
 
         let (dimension, value) = fold.trim().split_once("=").expect("malformed split");
@@ -54,8 +78,24 @@ fn dots_visible_after_folds(s: &str, limit: usize) -> usize {
             other => panic!("Unknown dimension {}", other),
         }
     }
+}
 
-    grid.len()
+fn print_grid(grid: &Grid) {
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    for &(x, y) in grid {
+        max_x = max(x, max_x);
+        max_y = max(y, max_y);
+    }
+
+    for y in 0..=max_y {
+        for x in 0..=max_x {
+            let c = if grid.contains(&(x, y)) { '#' } else { ' ' };
+            print!("{c}");
+        }
+        println!()
+    }
 }
 
 #[cfg(test)]
